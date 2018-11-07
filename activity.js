2d2612cec800de11a1de1114d96da63e62bb7380
Alkis Georgopoulos <alkisg@gmail.com>
2018-11-07 08:33:28 +0200

    Add a fullscreen button.

--- activity.js
+++ activity.js
@@ -25,9 +25,28 @@ function ge(element) {
 }
 
 function onPrevious(event) {
+  initActivity();
 }
 
 function onNext(event) {
+  initActivity();
+}
+
+function onToggleFullScreen(event) {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen
+    || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen
+    || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if (!doc.fullscreenElement && !doc.mozFullScreenElement
+    && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+  } else {
+    cancelFullScreen.call(doc);
+  }
 }
 
 function onHome(event) {
@@ -42,56 +61,12 @@ function onAbout(event) {
   window.open('credits/index_DS_II.html');
 }
 
-function onResize() {
-  var w = window.innerWidth;
-  var h = window.innerHeight;
-  if (w / h < 640 / 360) {
-    document.body.style.fontSize = sformat('{}px', 10 * w / 640);
-  } else {
-    document.body.style.fontSize = sformat('{}px', 10 * h / 360);
-  }
-}
-
-function addEvents() {
-  document.body.onresize = onResize;
-  ge('bar_previous').onclick = onPrevious;
-  ge('bar_next').onclick = onNext;
-  ge('bar_home').onclick = onHome;
-  ge('bar_help').onclick = onHelp;
-  ge('bar_about').onclick = onAbout;
-  ge('mainCanvas').onmouseover = onMouseOver;
-  ge('mainCanvas').onmouseout = onMouseOut;
-  ge('mainCanvas').onmousemove = onMouseMove;
-  ge('mainCanvas').ontouchstart = onTouchStart;
-  ge('mainCanvas').ontouchmove = onTouchMove;
-  ge('mainCanvas').ontouchend = onTouchEnd;
-}
-
-function initActivity() {
-  var ctc;
-  var img;
-
-  if (!act) {  // first run
-    addEvents();
-  }
-  act = {
-    pt: { x: -1, y: -1 },
-    thumb: ge('thumbCanvas'),
-    canvas: ge('mainCanvas'),
-  };
-  ctc = act.canvas.getContext('2d');
-  img = ge('foreground');
-  ctc.globalAlpha = 0.8;
-  ctc.drawImage(img, 0, 0, 800, 600);
-  onResize();
-}
-
 function drawLine(pt1, pt2) {
   var ctm;  // context for maincanvas
   var ctt;  // context for thumbcanvas
-  var sum = 0;
+  var sum = 0;  // sum of all thumbcanvas alpha channel bytes
   var i;
-  var fast = true;
+  var method = 1;
   var imd;
   var percent;
 
@@ -113,16 +88,23 @@ function drawLine(pt1, pt2) {
 
   ctt = act.thumb.getContext('2d');
   // drawImage is a bit slow on Android WebView
-  if (fast) {
+  if (method === 1) {
+    ctt.globalCompositeOperation = 'source-over';
     ctt.beginPath();
     ctt.lineWidth = 1;
     ctt.strokeStyle = 'rgb(255,255,255)';
     ctt.moveTo(pt1.x / 100, pt1.y / 100);
     ctt.lineTo(pt2.x / 100, pt2.y / 100);
     ctt.stroke();
-  } else {
-    ctt.clearRect(0, 0, ctt.canvas.width, ctt.canvas.height);
+  } else if (method === 2) {
+    ctt.globalCompositeOperation = 'copy';
     ctt.drawImage(act.canvas, 0, 0, ctt.canvas.width, ctt.canvas.height);
+  } else {
+    ctt.globalCompositeOperation = 'copy';
+    ctt.drawImage(act.canvas, 0, 0, ctt.canvas.width, ctt.canvas.height);
+    ctt.globalCompositeOperation = 'source-out';
+    ctt.fillStyle = '#ffffff';
+    ctt.fillRect(0, 0, ctt.canvas.width, ctt.canvas.height);
   }
 
   imd = ctt.getImageData(0, 0, ctt.canvas.width, ctt.canvas.height);
@@ -131,7 +113,7 @@ function drawLine(pt1, pt2) {
     sum += imd.data[i];
   }
   percent = Math.round(4 * 100 * sum / (255 * imd.data.length));
-  if (!fast) {
+  if (method === 2) {
     percent = 100 - percent;
   }
   ge('percent').innerHTML = sformat('{}%', percent);
@@ -189,5 +171,55 @@ function onTouchEnd(event) {
   touchToMouseEvent(event, 'mouseup');
 }
 
-document.onload = onResize;
+function onResize() {
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  if (w / h < 640 / 360) {
+    document.body.style.fontSize = sformat('{}px', 10 * w / 640);
+  } else {
+    document.body.style.fontSize = sformat('{}px', 10 * h / 360);
+  }
+}
+
+function addEvents() {
+  document.body.onresize = onResize;
+  ge('bar_previous').onclick = onPrevious;
+  ge('bar_next').onclick = onNext;
+  ge('bar_home').onclick = onHome;
+  ge('bar_fullscreen').onclick = onToggleFullScreen;
+  ge('bar_help').onclick = onHelp;
+  ge('bar_about').onclick = onAbout;
+  ge('mainCanvas').onmouseover = onMouseOver;
+  ge('mainCanvas').onmouseout = onMouseOut;
+  ge('mainCanvas').onmousemove = onMouseMove;
+  ge('mainCanvas').ontouchstart = onTouchStart;
+  ge('mainCanvas').ontouchmove = onTouchMove;
+  ge('mainCanvas').ontouchend = onTouchEnd;
+}
+
+function initActivity() {
+  var ctc;
+  var ctt;
+  var img;
+
+  if (!act) {  // first run
+    addEvents();
+  }
+  act = {
+    pt: { x: -1, y: -1 },
+    thumb: ge('thumbCanvas'),
+    canvas: ge('mainCanvas'),
+  };
+  ctc = act.canvas.getContext('2d');
+  ctc.globalCompositeOperation = 'copy';
+  ctc.globalAlpha = 0.8;
+  img = ge('foreground');
+  ctc.drawImage(img, 0, 0, ctc.canvas.width, ctc.canvas.height);
+  ctt = act.thumb.getContext('2d');
+  ctt.globalCompositeOperation = 'copy';
+  ctt.clearRect(0, 0, ctt.canvas.width, ctt.canvas.height);
+  ge('percent').innerHTML = '0%';
+  onResize();
+}
+
 window.onload = initActivity;
